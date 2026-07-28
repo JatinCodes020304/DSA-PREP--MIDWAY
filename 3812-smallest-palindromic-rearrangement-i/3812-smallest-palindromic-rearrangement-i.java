3414842fd8107fd9e959188160aaab7863a9d4bb
Jatinguleria <jatinguleria020304@gmail.com>
2026-07-28 10:41:57 +0530

    3812-SMALLEST-PALINDROMIC-REARRANGEMENT-I Stats: Time: 20 ms (97.19%), Space: 47.9 MB (88.35%) - LeetHub

--- 3812-smallest-palindromic-rearrangement-i/3812-smallest-palindromic-rearrangement-i.java
+++ 3812-smallest-palindromic-rearrangement-i/3812-smallest-palindromic-rearrangement-i.java
@@ -1,19 +1,21 @@
 class Solution {
     public String smallestPalindrome(String s) {
-        int n = s.length(), j = 0;
-        int[] freq = new int[26];
-        char[] chars = s.toCharArray();
-
-        for (int i = 0; i < n >> 1; i++)
-            freq[(chars[i] & 31) - 1]++;
-
-        for (int i = 0; i < 26; i++) {
-            while (freq[i]-- > 0) {
-                chars[j] = (char) (97 + i);
-                chars[n - 1 - j++] = (char) (97 + i);
+        int n=s.length();
+        int j=0;
+        int  a[]=new int[26];
+        char b[]=s.toCharArray();
+        for(int i=0;i<n/2;i++){
+            a[b[i]-'a']++;
+        }
+        for(int i=0;i<26;i++){
+            while(a[i]>0){
+                b[j]=(char)('a'+i);
+                b[n-1-j]=(char)('a'+i);
+                j++;
+                a[i]--;
             }
         }
-
-        return new String(chars);
+        return new String(b);
+        // for (int i = 0; i < n >>
     }
 }
